cf62665beecc16f0792ed85384062d93f43f97fa
Micheal Shi <86520894+sjh0354@users.noreply.github.com>
2022-12-21 15:41:37 +0800

    All tasks finished

--- lab_cow/kalloc.c
+++ lab_cow/kalloc.c
@@ -156,3 +156,26 @@ cow_alloc(pagetable_t pagetable, uint64 va) {
   kfree((void *)pa);
   return 0;
 } 
+
+int
+copyout(pagetable_t pagetable, uint64 dstva, char *src, uint64 len)
+{
+  uint64 n, va0, pa0;
+  
+  while(len > 0){
+    va0 = PGROUNDDOWN(dstva);
+    cow_alloc(pagetable, va0);
+    pa0 = walkaddr(pagetable, va0);
+    if(pa0 == 0)
+      return -1;
+    n = PGSIZE - (dstva - va0);
+    if(n > len)
+      n = len;
+    memmove((void *)(pa0 + (dstva - va0)), src, n);
+
+    len -= n;
+    src += n;
+    dstva = va0 + PGSIZE;
+  }
+  return 0;
+}
